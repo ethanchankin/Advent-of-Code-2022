f6634ade0b72876a587469959342f2df98c40318
Ethan Chankin <ethanchankin@gmail.com>
2022-12-07 10:35:38 -0600

    Simplified the mess I made a few hours ago a tiny bit

--- Day 2/pbm2/rps2.cpp
+++ Day 2/pbm2/rps2.cpp
@@ -11,7 +11,7 @@ int main(){
     while(getline(inFile, line)){
         char oppChar = line[0];
         char instruction = line[2];
-        total += ((oppChar - (((((instruction - 2) % 3) - 3) * -1) - 1)) % 3) + (((instruction - 1) % 3) * 3) + 1;
+        total += ((oppChar - ((((instruction - 2) % 3) - 2) * -1)) % 3) + (((instruction - 1) % 3) * 3) + 1;
     }
     cout << total << endl;
 }
